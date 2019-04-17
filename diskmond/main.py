8962f00643419208b92be3d45a5cd4240a7ab08c
Aleksandr Bogdanov <syn@syn.im>
2019-04-17 16:52:05 +0200

    Support time values

--- diskmond/main.py
+++ diskmond/main.py
@@ -1,9 +1,15 @@
 import os
+import re
 from time import sleep
+from datetime import timedelta
+import logging
 
 import click
 from pySMART import Device, Attribute, DeviceList
 from statsd.defaults.env import statsd
+
+logger = logging.getLogger(__name__)
+time_re = re.compile(r'^((?P<hours>[\.\d]+?)h\+)?((?P<minutes>[\.\d]+?)m\+)?((?P<seconds>[\.\d]+?)s)?$')
 
 
 @click.command()
@@ -21,7 +27,18 @@ def cli(interval):
                 assert isinstance(a, Attribute)
 
                 key = f'system.disk.smart.{a.num}_{a.name}.{dev.name}'
-                value = int(a.raw)
+
+                if 's' in a.raw:
+                    parts = time_re.match(a.raw)
+                    time_components = {name: float(param) for name, param in parts.groupdict().items() if param}
+                    value = timedelta(**time_components).total_seconds()
+                else:
+                    try:
+                        value = int(a.raw)
+                    except ValueError:
+                        logger.warning('cannot parse SMART raw value: %s', a.raw)
+                        continue
+
                 statsd.gauge(key, value)
         sleep(interval)
 
