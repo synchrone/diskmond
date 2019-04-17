f2319a54c2a22b8284e5bb51cce7f0adc5060034
Aleksandr Bogdanov <syn@syn.im>
2019-04-17 17:04:50 +0200

    Adding static files

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     py_modules=['diskmond'],
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_data={'diskmond': ['*']},
     install_requires=[
         'Click==7.0',
         'pysmart.smartx==0.3.9',
